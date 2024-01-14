b38a8047c11092e7cca5002592686cc915704333
FirstLordar <120566042+FirstLordar@users.noreply.github.com>
2024-01-14 15:07:07 +0300

    Первый коммит
    Проба добавления в массив персонажей Объектов, не выводится в карточку имя и описание, разобраться с undefined

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 let checked = document.querySelector('input[id="2"]:checked');
 let addButton = document.getElementById('addButton');
 let heroes = [['Axe','Tank'],['Crystale','support']];
+//Получение элементов формы
+const form = document.forms.addHero; // Получаем форму
+const nameInput = form.elements.heroName; // Получаем поле с именем
+const descriptionInput = form.elements.description; // Получаем поле с описанием
+const strInput = form.elements.str; // Получаем поле с уровнем силы
+const agiInput = form.elements.agi; // Получаем поле с уровнем ловкости
+const hpInput = form.elements.hp; // Получаем поле с уровнем здоровья
+const intInput = form.elements.int; // Получаем поле с уровнем интеллекта
 
 
 
@@ -8,7 +16,7 @@ function displayHeroes() {
     let heroesContainer = document.getElementById('heroesContainer');
     heroesContainer.innerHTML = '';
     for (i = 0; i < heroes.length; i++) {
-    let newElement = document.createElement('label');
+    let newElement = document.createElement('div');
     
     newElement.classList.add(`card__${i + 1}`);
     newElement.setAttribute('id', `card-${i + 1}`);
@@ -16,8 +24,8 @@ function displayHeroes() {
     newElement.innerHTML = `
     <img src="/img/hero_card.png" alt="hero_card" class="hero__card">
     <div class="hero__title">
-    <h2 class="hero__title-name">${heroes[i][0]}</h2>
-    <p>${heroes[i][1]}</p>
+    <h2 class="hero__title-name">${heroes[i][heroName]}</h2>
+    <p>${heroes[i][description]}</p>
     </div>
     `
     heroesContainer.appendChild(newElement);
@@ -27,11 +35,18 @@ function displayHeroes() {
 }
 
 function addHero () {
-    let nameInput = document.getElementById('heroName');
-    let classInput = document.getElementById('heroClass');
     if (nameInput.value != '') {
-        if (classInput.value != '') {
-            let newHero = [nameInput.value, classInput.value]
+        if (descriptionInput.value != '') {
+            let newHero = {
+              heroName: nameInput.value,
+              description: descriptionInput.value,
+              str: strInput.value,
+              agi: agiInput.value,
+              hp: hpInput.value,
+              int: intInput.value,
+                // укажем тестовую почту для регистрации карточки
+                studentEmail: "student@yandex.ru",
+            };
             heroes.push(newHero);
         } else {
             alert('Введите класс персонажа')
@@ -42,13 +57,48 @@ function addHero () {
    displayHeroes()
     
     nameInput.value = '';
-    classInput.value = '';
+    descriptionInput.value = '';
 }
 
 
 
 
 addButton.onclick = addHero
+
+// form.addEventListener('submit', function (evt) {
+//     // Отменяем стандартное поведение
+//     evt.preventDefault();
+  
+//     // Формируем данные и запрос
+//   });
+
+//   let newHeroJSON = JSON.stringify(newHero);
+
+//   // Создаём объект с новым персонажем и записываем данные
+
+
+// Используем api нашего сервера 
+fetch("https://api-code.practicum-team.ru/heroes", {
+    method: "POST", // POST нужен для сохранения и записи данных
+  body: newHeroJSON, // Тело запроса в JSON-формате
+  headers: {
+      // Добавляем необходимые заголовки
+      "Content-type": "application/json; charset=UTF-8",
+    },
+})
+    .then((response) => response.json())
+    // Смотрим ответ сервера на наш запрос
+  .then((data) => console.log(data))
+
+
+  form.addEventListener('submit', function (evt) {
+    // Отменяем стандартное поведение
+    evt.preventDefault();
+    // Формируем данные и запрос...
+  
+      // Сбрасываем все поля
+      form.reset();
+  });
 
 
 
